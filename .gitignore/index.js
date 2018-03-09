14a95c4303cc45b53ca5cab5fabdaf898410a5aa
chaussettesale <37185103+chaussettesale@users.noreply.github.com>
2018-03-09 11:43:46 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,6 +15,37 @@ const Discord = require('discord.js')
                             message.channel.send('Démons...et merveilles !\nEnfer...més dans nos coeurs \nDevil adversité \nEnfer...vents défenseurs \nDémons...tagnes jeunes \nSatan...père à nos yeux \nL\'incube...ation du temps \nMal traitant nos aïeux ! \nC\'est l\'antéchrist Staline \nPrières que j\'ai lu- \n-cifer tiles et si riches \nQu\'on damne nos abus !')
                             }
                           })
+
+bot.on('message', message => {
+                            if (message.content === '!nomo') {
+                            message.channel.send('Bon... Saurais-tu soigner nos maux ?')
+                             }
+                             })
+
+bot.on('message', message => {
+                            if (message.content === 'Qui est-ce ?') {
+                            message.channel.send('Comment ça qui est-ce ?')
+                             }
+                             })
+
+bot.on('message', message => {
+                            if (message.content === 'Je me doute bien. Ce doit être un pseudo... Tendancieux qui plus est, hein ?') {
+                            message.channel.send('Mais non. Nos maux, c\'est nos bobos quoi !')
+                             }
+                             })
+
+bot.on('message', message => {
+                            if (message.content === 'Ah ! Il a un autre surnom ?') {
+                            message.channel.send('Mais non bordel !')
+                             }
+                             })
+
+bot.on('message', message => {
+                            if (message.content === 'Bordel ? C\'est dur à porter ça. J\'avais un ami qu\'on appelait couil...') {
+                            message.channel.send('Un mal, des maux! Comme un ail, des aulx...')
+                             }
+                             })
+
       bot.on('message', message => {
                             if (message.content === '!helpw') {
                             message.channel.send(' Pour me controler il faut taper l\'une des commandes suivantes:\n\n**__Catégorie Stratégie:__**\n\n**__Catégorie Fun:__**\n\n**__Catégorie RDA:__**\n\n ```!chanteW ``` ')
